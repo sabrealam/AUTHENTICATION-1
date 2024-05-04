2946c0d6afed1c3208b1d5e24a5867156c6d8891
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-05-04 16:11:46 +0530

    1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const dbFunction = require("./config/db");
+const cors = require("cors");
  
 const cors = require("cors");
 const app = express();
